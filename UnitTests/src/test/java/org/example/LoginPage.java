39bde91605a752616af18a5f1f166d7cccd7b1bf
AndreyMoh <karenegoryanreal@gmail.com>
2023-06-30 16:29:54 +0300

    Second commit

---
+++ UnitTests/src/test/java/org/example/LoginPage.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    @FindBy(xpath = "//*[@id=\"passp-field-login\"]") private WebElement loginField;
+    @FindBy(xpath = "//*[contains(text(), \"Войти\")]") private WebElement loginBtn;
+    @FindBy(xpath = "//*[@id=\"passp-field-passwd\"]") private WebElement passwdField;
+
+    public void inputLogin(String login) {
+        loginField.sendKeys(login); }
+    public void inputPasswd(String passwd) { passwdField.sendKeys(passwd); }
+    public void clickLoginBtn() { loginBtn.click(); }
+}
