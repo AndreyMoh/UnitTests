c10971ad66a6701603dd2c74dc86adf5285c1cf8
AndreyMoh <karenegoryanreal@gmail.com>
2023-06-30 18:02:31 +0300

    Last commit in 30.05.2023

--- UnitTests/src/test/java/org/example/LoginTest.java
+++ UnitTests/src/test/java/org/example/LoginTest.java
@@ -1,20 +1,66 @@
 package org.example;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
+    public static LoginPage loginPage;
+    public static ProfilePage profilePage;
+    public static WebDriver driver;
+
+    /**
+     * осуществление первоначальной настройки
+     */
     @BeforeClass
     public static void setup() {
+
         System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
 
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
+        profilePage = new ProfilePage(driver);
 
         driver.manage().window().maximize();
+
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
         driver.get(ConfProperties.getProperty("loginpage"));
+    }
+
+    /**
+     * тестовый метод для осуществления аутентификации
+     */
+    @Test
+    public void loginTest() {
+
+// loginPage.inputLogin(ConfProperties.getProperty("login"));
+//
+// loginPage.clickLoginBtn();
+//
+// loginPage.inputPasswd(ConfProperties.getProperty("password"));
+//
+// loginPage.clickLoginBtn();
+
+        profilePage.entryMainMenu();
+        profilePage.entryMail();
+        profilePage.entryMenu();
+
+        String user = profilePage.getUserName();
+        System.out.println(user);
+    }
+
+    /**
+     * осуществление выхода из аккаунта с последующим закрытием окна браузера
+     */
+    @AfterClass
+    public static void tearDown() {
+        profilePage.entryMenu();
+        profilePage.userLogout();
+        driver.quit();
     }
 }
---
+++ UnitTests/src/test/java/org/example/ProfilePage.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProfilePage {
+
+    //*[@id="__next"]/div/header/div[4]/div/a[1]/div/div[1]
+    public WebDriver driver;
+
+    public ProfilePage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    /**
+     * определение локатора меню пользователя
+     */
+    @FindBy(xpath = "//*[contains(@class, 'ListItem MenuItem MenuItem_mail')]")
+    private WebElement mailItem;
+    @FindBy(xpath = "//*[contains(@class, 'UserID-Avatar')]")
+    private WebElement mainMenu;
+
+    @FindBy(xpath = "//*[contains(@class, 'user-account__name')]")
+    private WebElement userMenu;
+
+    @FindBy(xpath = "//*[contains(@class, 'menu__item menu__item_type_link count-me legouser__menu-item legouser__menu-item_action_exit legouser__menu-item legouser__menu-item_action_exit')]")
+    private WebElement logoutBtn;
+
+    public String getUserName() {
+        return userMenu.getText();
+    }
+
+    public void entryMenu() {
+        userMenu.click();
+    }
+    public void entryMainMenu() {
+        mainMenu.click();
+    }
+    public void entryMail() {
+        mailItem.click();
+    }
+
+    public void userLogout() {
+        logoutBtn.click();
+    }
+
+}
